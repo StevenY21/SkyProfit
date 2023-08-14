2c6f64aa2d9aa37815f75cbc2f3cf9ed43973252
StevenY21 <93829301+StevenY21@users.noreply.github.com>
2023-08-14 08:53:34 +0000

    recategorizing global variables

--- src/globals.py
+++ src/globals.py
@@ -3,8 +3,6 @@ import re, json, requests
 import time
 import aiohttp
 import asyncio
-import msgspec
-import pandas as pd
 # for final output of commands
 start = time.time()
 finalOutput = discord.Embed(title="  ", colour=0x1978E3)
@@ -24,6 +22,8 @@ SB_ITEMS_DATA = asyncio.run(
 SB_BZ_DATA = asyncio.run(
   req_data("https://api.hypixel.net/skyblock/bazaar"))["products"]
 # Vanilla items that have uses in skyblock
+SB_CATEGORIES = {'REFORGE_STONE': 'bz', 'N/A': 'tools and misc', 'SWORD': 'weapon', 'BOOTS': 'armor', 'BELT': 'misc', 'NECKLACE': 'misc', 'GLOVES': 'misc', 'HELMET': 'armor', 'CHESTPLATE': 'armor', 'ACCESSORY': "accessories", 'CLOAK': 'misc', 'LEGGINGS': 'misc', 'AXE': 'misc', 'HOE': 'misc', 'COSMETIC': 'misc', 'MEMENTO': 'misc', 'PET_ITEM': 'blocks and tools and misc', 'BAIT': 'bz', 'FISHING_ROD': 'tool', 'WAND': 'tools' , 'PORTAL': 'misc', 'BOW': "weapon", 'NONE': 'tools and misc', 'DUNGEON_PASS': 'none', 'ARROW': 'none', 'SPADE': 'tools', 'PICKAXE': 'tools', 'DEPLOYABLE': 'tools', 'DRILL': 'tools', 'SHEARS': 'tools and misc', 'BRACELET': 'misc', 'GAUNTLET': 'misc', 'LONGSWORD': 'weapon', 'TRAVEL_SCROLL': 'misc', 'ARROW_POISON': 'bz', 'FISHING_WEAPON': 'tools'}
+catDict = {"weapon": False, "armor": False, "accessories": True, "consumables": True, "blocks": True, "tools": True, "misc": True}
 USEFUL_VANILLA_ITEMS = [
   "Lapis Lazuli", "Coal", "Diamond", "Redstone", "Gold Ingot", "Iron Ingot",
   "Wheat", "Cobblestone", "Bone", "Emerald", "Slimeball", "Snow Block", "Ice",
@@ -36,28 +36,12 @@ USEFUL_VANILLA_ITEMS = [
   "Carrot", "Red Mushroom", "Brown Mushroom", "Hay Bale"
 ]
 USELESS_VANILLA_ITEMS = []
-SB_ITEMS_DICT = {}  # key: item name, value: item id
+SB_UNIQUE_CAT = {}  # all categories in hypixel sb items data
+SB_UNIQUE_TIER = {"SUPREME": False} # all tiers in hypixel sb items data
+# key: item id, value: {'item name':, 'item'}
+SB_ID_DICT = {}  # key: item id, value: item name
 SB_NAME_FIX = {
 }  # key: item name in lower case, value: item name's proper uppercase form
-SB_ID_DICT = {}  # key: item id, value: item name
-SB_MAT_DICT = {}  # key: item id, value: item material
-SB_CAT_DICT = {} # key: item name, value: item category
-SB_UNIQUE_CAT = {}  # all categories in hypixel sb items data
-SB_TIER_DICT = {} # key: item name, value: item tier
-SB_UNIQUE_TIER = {"SUPREME": False} # all tiers in hypixel sb items data
-SB_AH_DICT = {}  # key: item name, value: true or false
-SB_BZ_DICT = {}  # key: item name, value: true or false
-SB_CRAFTPROFIT_DATA = {
-}  # key: valid craftprofit item id, value: item crafting recipe
-SB_FORGE_ITEMS_DATA = {
-}  # key: valid forgeprofit item id, value: item forge recipe
-SB_NONCRAFTABLES_LIST = [
-]  # list of all items that cannot be crafted or forged
-SB_NONCRAFTABLES_DICT = {}  # key: item name, value: true or false
-SB_INVALID_ITEMS = []  # just in case if some wacky items appear
-SB_MINIONS_LIST = []  # all minions with all tiers
-SB_SOULBOUND_LIST = []  # all soulbound items
-SB_SOULBOUND_DICT = {}  #key: item name, value: true or false
 BASE_ITEMS = [
   "Lapis Lazuli", "Coal", "Diamond", "Redstone", "Gold Ingot", "Iron Ingot",
   "Wheat", "Cobblestone", "Bone", "Emerald", "Slimeball", "Snow Block",
@@ -242,6 +226,241 @@ ABICASE_DICT = {"Sumsung© G3 Abicase": 15000,
   "Lighter Blue™ Abicase": 17000,}
 # Stars
 SB_STARS = ["✪✪✪✪✪", "➊", "➋", "➌", "➍", "➎"]
+SB_ITEM_DICT = {} # my own version of hypixel sb item data set
+"""
+example of an item in SB_ITEM_DICT
+[item ID]: {
+  'name': ,
+  'material': ,
+  'tier': ,
+  'category': ,
+  'soulbound': ,
+  'furniture': ,
+  'generator': ,
+  'in_bz': ,
+  'in_ah': ,
+  'ah_category': ,
+  'base_item': ,  
+  'vanilla': ,
+  'ah_vanilla': #vanilla items that are meant to be filtered out as they should never be bough directly from bz or ah
+}
+"""
+  
+for item in SB_ITEMS_DATA["items"]:
+  itemID = item["id"]
+  SB_ITEM_DICT[itemID] = {}
+  # name 
+  itemName = item["name"]
+  SB_ITEM_DICT[itemID]['name'] = itemName
+  # for special dictionaries
+  SB_ID_DICT[itemID] = itemName
+  SB_NAME_FIX[itemName.lower()] = itemName
+  # processing some enrichments that I am too lazy to type out
+  if "Enrichment" in itemName and "Accessory" not in itemName:
+    SB_BITS_SHOP_1[itemName] = 5000
+    SB_BITS_SHOP_2[itemName] = 5000
+  # material
+  itemMat = item["material"]
+  SB_ITEM_DICT[itemID]['material'] = itemMat
+  # tier 
+  itemTier = ""
+  if item.get('tier') != None:
+    itemTier = item["tier"]
+  else:
+    itemTier = "UNTIERED"
+  SB_ITEM_DICT[itemID]['tier'] = itemTier
+  SB_UNIQUE_TIER[itemTier] = False
+  # category
+  itemCat = item.get('category')
+  if item.get('category') != None:
+    SB_UNIQUE_CAT[itemCat] = False
+    SB_ITEM_DICT[itemID]['category'] = itemCat
+  else:
+    SB_UNIQUE_CAT["N/A"] = False
+    SB_ITEM_DICT[itemID]['category'] = 'N/A'
+  itemSB = item.get("soulbound")
+  if itemSB != None:
+    SB_ITEM_DICT[itemID]['soulbound'] = itemSB
+  else:
+    SB_ITEM_DICT[itemID]['soulbound'] = 'N/A'
+  # furniture
+  itemFurn = item.get("furniture")
+  if itemFurn != None:
+    SB_ITEM_DICT[itemID]['furniture'] = itemFurn
+  else:
+    SB_ITEM_DICT[itemID]['furniture'] = 'N/A'
+  # generator  
+  itemGen = item.get('generator')
+  if itemGen != None:
+    SB_ITEM_DICT[itemID]['generator'] = itemGen
+  else:
+    SB_ITEM_DICT[itemID]['generator'] = 'N/A'
+  # in_bz and in_ah
+  if SB_ITEM_DICT[itemID]['soulbound'] == 'N/A' and SB_ITEM_DICT[itemID]['generator'] == 'N/A' and 'sack' not in itemName:
+    try:
+      test = SB_BZ_DATA[itemID]
+      SB_ITEM_DICT[itemID]['in_bz'] = True
+      SB_ITEM_DICT[itemID]['in_ah'] = False
+    except:
+      SB_ITEM_DICT[itemID]['in_bz'] = False
+      SB_ITEM_DICT[itemID]['in_ah'] = True
+  else:
+    SB_ITEM_DICT[itemID]['in_bz'] = False
+    if itemName == 'Pocket Sack-in-a-Sack':
+      SB_ITEM_DICT[itemID]['in_ah'] = True
+    else:
+      SB_ITEM_DICT[itemID]['in_ah'] = False
+  # ah_category
+  SB_ITEM_DICT[itemID]['ah_category'] = SB_CATEGORIES[SB_ITEM_DICT[itemID]['category']]
+  #base_item gonna be set to false initially
+  SB_ITEM_DICT[itemID]['base_item'] = False
+  # vanilla and 'ah_vanilla'
+  if itemID == itemMat or 'INK_SAC:' in itemID or 'LOG' in itemID:
+    SB_ITEM_DICT[itemID]['vanilla'] = True
+    if SB_ITEM_DICT[itemID]['in_ah'] == True:
+      SB_ITEM_DICT[itemID]['ah_vanilla'] == True
+    else:
+      SB_ITEM_DICT[itemID]['ah_vanilla'] == False
+  else:
+    SB_ITEM_DICT[itemID]['vanilla'] = False
+  if "Stairs" in itemName or "Fence" in itemName or "STONE:" in itemID or "Slab" in itemName or "Planks" in itemName or "Rail" in itemName:
+    SB_ITEM_DICT[itemID]['vanilla'] = True
+    SB_ITEM_DICT[itemID]['ah_vanilla'] = True
+  else:
+    SB_ITEM_DICT[itemID]['ah_vanilla'] = False
+  BASE_ITEMS_DICT[itemName] = False
+# manually fix some strange items
+SB_ITEM_DICT["SPECKLED_MELON"]['ah_vanilla'] = False
+SB_ITEM_DICT["SPECKLED_MELON"]['in_ah'] = False
+SB_ITEM_DICT["GOLDEN_APPLE:1"]['ah_vanilla'] = True
+SB_ID_DICT["JUMBO_BACKPACK"] = "Jumbo Backpack"
+SB_ITEM_DICT["JUMBO_BACKPACK"] = {
+  'name': "Jumbo Backpack" ,
+  'material': "SKULL_ITEM",
+  'tier': 'LEGENDARY',
+  'category': 'N/A',
+  'soulbound': False,
+  'furniture': 'N/A',
+  'generator': 'N/A',
+  'in_bz': False,
+  'in_ah': True,
+  'ah_category': 'tools and misc',
+  'base_item': False,  
+  'vanilla': False,
+  'ah_vanilla': False
+}
+SB_ID_DICT["GREATER_BACKPACK"] = "Greater Backpack"
+SB_ITEM_DICT["GREATER_BACKPACK"] = {
+  'name': "Greater Backpack" ,
+  'material': "SKULL_ITEM",
+  'tier': "EPIC",
+  'category': 'N/A',
+  'soulbound': False,
+  'furniture': 'N/A',
+  'generator': 'N/A',
+  'in_bz': False,
+  'in_ah': True,
+  'ah_category': 'tools and misc',
+  'base_item': False,  
+  'vanilla': False,
+  'ah_vanilla': False
+}
+SB_ID_DICT["LARGE_BACKPACK"] = "Large Backpack"
+SB_ITEM_DICT["LARGE_BACKPACK"] = {
+  'name': "Large Backpack" ,
+  'material': "SKULL_ITEM",
+  'tier': "EPIC",
+  'category': 'N/A',
+  'soulbound': False,
+  'furniture': 'N/A',
+  'generator': 'N/A',
+  'in_bz': False,
+  'in_ah': True,
+  'ah_category': 'tools and misc',
+  'base_item': False,  
+  'vanilla': False,
+  'ah_vanilla': False
+}
+SB_ID_DICT["MEDIUM_BACKPACK"] = "Medium Backpack"
+SB_ITEM_DICT["MEDIUM_BACKPACK"] = {
+  'name': "Medium Backpack" ,
+  'material': "SKULL_ITEM",
+  'tier': "RARE",
+  'category': 'N/A',
+  'soulbound': False,
+  'furniture': 'N/A',
+  'generator': 'N/A',
+  'in_bz': False,
+  'in_ah': True,
+  'ah_category': 'tools and misc',
+  'base_item': False,  
+  'vanilla': False,
+  'ah_vanilla': False
+}
+SB_ID_DICT["SMALL_BACKPACK"] = "Small Backpack"
+SB_ITEM_DICT["SMALL_BACKPACK"] = {
+  'name': "Small Backpack" ,
+  'material': "SKULL_ITEM",
+  'tier': "UNCOMMON",
+  'category': 'N/A',
+  'soulbound': False,
+  'furniture': 'N/A',
+  'generator': 'N/A',
+  'in_bz': False,
+  'in_ah': True,
+  'ah_category': 'tools and misc',
+  'base_item': False,  
+  'vanilla': False,
+  'ah_vanilla': False
+}
+# Processing excluded items and base items:
+for i in BASE_ITEMS:
+  SB_ID_DICT[itemID] = itemName
+  BASE_ITEMS_DICT[i] = True
+for i in EXCLUDED_ITEMS:
+  EXCLUDED_ITEMS_DICT[i] = True
+#Processing Enchants Here:
+for enchant in BITS_ENCHANTS_LIST:
+  SB_ITEMS_DICT[enchant] = f"ENCHANTMENT_{enchant.upper()}_1"
+  SB_ID_DICT[f"ENCHANTMENT_{enchant.upper()}_1"] = enchant
+  SB_SOULBOUND_DICT[enchant] = False
+  EXCLUDED_ITEMS_DICT[enchant] = False
+  BASE_ITEMS_DICT[enchant] = False
+  SB_BZ_DICT[f"ENCHANTMENT_{enchant.upper()}_1"] = True
+  SB_AH_DICT[enchant] = False
+for abicase in ABICASE_DICT:
+  SB_CAT_DICT[abicase] = "ACCESSORY"
+  SB_TIER_DICT[abicase] = "RARE"
+#create the filters for cookieprofit
+SB_BITS_FILTER = {
+  "None": SB_BITS_SHOP_1,
+  "No Abicase": SB_BITS_SHOP_2,
+  "No Enrichment": SB_BITS_SHOP_3,
+  "No Abicase and Enrichment": SB_BITS_SHOP_4
+}
+
+SB_ITEMS_DICT = {}  # key: item name, value: item id
+SB_NAME_FIX = {
+}  # key: item name in lower case, value: item name's proper uppercase form
+SB_ID_DICT = {}  # key: item id, value: item name
+SB_MAT_DICT = {}  # key: item id, value: item material
+SB_CAT_DICT = {} # key: item name, value: item category
+SB_UNIQUE_CAT = {}  # all categories in hypixel sb items data
+SB_TIER_DICT = {} # key: item name, value: item tier
+SB_UNIQUE_TIER = {"SUPREME": False} # all tiers in hypixel sb items data
+SB_AH_DICT = {}  # key: item name, value: true or false
+SB_BZ_DICT = {}  # key: item name, value: true or false
+SB_CRAFTPROFIT_DATA = {
+}  # key: valid craftprofit item id, value: item crafting recipe
+SB_FORGE_ITEMS_DATA = {
+}  # key: valid forgeprofit item id, value: item forge recipe
+SB_NONCRAFTABLES_LIST = [
+]  # list of all items that cannot be crafted or forged
+SB_NONCRAFTABLES_DICT = {}  # key: item name, value: true or false
+SB_INVALID_ITEMS = []  # just in case if some wacky items appear
+SB_MINIONS_LIST = []  # all minions with all tiers
+SB_SOULBOUND_LIST = []  # all soulbound items
+SB_SOULBOUND_DICT = {}  #key: item name, value: true or false
 # Sorting out all the items
 for item in SB_ITEMS_DATA["items"]:
   itemName = item["name"]
@@ -307,7 +526,7 @@ for item in SB_ITEMS_DATA["items"]:
   SB_ID_DICT[itemID] = itemName
   SB_MAT_DICT[itemID] = itemMat
   SB_NAME_FIX[itemName.lower()] = itemName
-  if "Stairs" in itemName or "Fence" in itemName or "Chiseled" in itemName or "Granite" in itemName or "Diorite" in itemName or "Andesite" in itemName or "Slab" in itemName or "Planks" in itemName or "Rail" in itemName:
+  if "Stairs" in itemName or "Fence" in itemName or "Chiseled" in itemName or "Stone:" in itemID or "Slab" in itemName or "Planks" in itemName or "Rail" in itemName:
 
     EXCLUDED_ITEMS_DICT[itemName] = True
   else:
