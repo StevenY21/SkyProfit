6045054cf5173c5c49cc844db8a6e2556eb45333
StevenY21 <93829301+StevenY21@users.noreply.github.com>
2023-08-24 07:59:51 +0000

    fixed the ACTUAL redundant code

--- src/functions.py
+++ src/functions.py
@@ -62,19 +62,8 @@ def get_item_recipe(itemName):
       if recipeData[section] != '':
         item = recipeData[section].split(':')
         itemID = item[0]
-        fixedID = ""
-        itemName2 = ""
-        itemID.replace("-", ":")
-        if "-" in itemID:
-          for chr in itemID:
-            if chr == '-':
-              fixedID += ':'
-            else:
-              fixedID += chr
-
-          itemName2 = SB_ITEM_DATA[fixedID]['name']
-        else:
-          itemName2 = SB_ITEM_DATA[itemID]['name']
+        fixedID = itemID.replace("-", ":")
+        itemName2 = SB_ITEM_DATA[fixedID]['name']
         if itemName2 in properRecipe:
           properRecipe[itemName2] += int(item[1])
         else:
